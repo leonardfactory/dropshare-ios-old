d7d1d589e2a660067978d4b291e5b4f4de5e55a4
Leonardo Ascione <rockerlion@gmail.com>
2013-02-23 15:27:08 +0100

    SidePanel working & Fixed storyboard

---
+++ Shabaka/controllers/UIViewController+SidePanel.m
@@ -0,0 +1,34 @@
+//
+//  UIViewController+SidePanel.m
+//  Ramses
+//
+//  Created by Leonardo on 23/02/13.
+//  Copyright (c) 2013 Dropshare. All rights reserved.
+//
+
+#import "UIViewController+SidePanel.h"
+
+@implementation UIViewController (SidePanel)
+
+- (DSSidePanelController *) sidePanelController
+{
+    UIViewController *iter = self.parentViewController;
+    while (iter)
+    {
+        if ([iter isKindOfClass:[DSSidePanelController class]])
+		{
+            return (DSSidePanelController *) iter;
+        }
+		else if (iter.parentViewController && iter.parentViewController != iter)
+		{
+            iter = iter.parentViewController;
+        }
+		else
+		{
+            iter = nil;
+        }
+    }
+    return nil;
+}
+
+@end
