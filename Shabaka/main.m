f223623a5edaf1d729f340b1411f33856e2be099
leonardfactory <me@leonardfactory.com>
2013-02-23 14:33:16 +0100

    First Commit

---
+++ Shabaka/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Shabaka
+//
+//  Created by Francesco on 23/02/13.
+//  Copyright (c) 2013 Dropshare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DSAppDelegate class]));
+	}
+}
