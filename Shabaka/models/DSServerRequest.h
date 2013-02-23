29724395f212ef0849e92131f3e2449957bfb12f
leonardfactory <me@leonardfactory.com>
2013-02-23 17:26:36 +0100

    afnetworking

--- Shabaka/models/DSServerRequest.h
+++ Shabaka/models/DSServerRequest.h
@@ -8,10 +8,13 @@
 
 #import <Foundation/Foundation.h>
 #import "AFJSONRequestOperation.h"
+#define SERVERURL @"http://192.168.0.102"
 
 @interface DSServerRequest : NSObject
 
-- (DSServerRequest *) initWithMethod:(NSString *)method withURL:(NSString *)url withParams:(NSDictionary *)params withBody:(NSDictionary *)body;
+@property (strong, nonatomic) NSMutableURLRequest *request;
+
+- (DSServerRequest *) initWithMethod:(NSString *)method withEntity:(NSString *)entity withIdentifier:(NSString *)identifier withParams:(NSDictionary *)params withBody:(NSDictionary *)body;
 
 - (void) doAndIfSuccess:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, id JSON))success elseIfFailure:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON))failure;
 
