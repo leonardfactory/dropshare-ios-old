87897f360ec3e13630ab7a1613b4215e2ccd47be
Leonardo Ascione <rockerlion@gmail.com>
2013-02-23 15:58:22 +0100

    LeftSidePanel Added & Map

---
+++ Shabaka/controllers/DSLeftSidePanelViewController.m
@@ -0,0 +1,48 @@
+//
+//  DSLeftSidePanelViewController.m
+//  Shabaka
+//
+//  Created by Leonardo Ascione on 23/02/13.
+//  Copyright (c) 2013 Dropshare. All rights reserved.
+//
+
+#import "DSSidePanelController.h"
+#import "UIViewController+SidePanel.h"
+
+#import "DSLeftSidePanelViewController.h"
+
+@interface DSLeftSidePanelViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *menuButtonDiscover;
+
+@end
+
+@implementation DSLeftSidePanelViewController
+
+// @todo: aggiungere initWithCoder
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+	// Codice per personalizzare l'onclick background
+	UIImage *menuButtonBgPressedPattern = [[UIImage imageNamed:@"menuButtonBgPressed"] resizableImageWithCapInsets:UIEdgeInsetsZero];
+    [self.menuButtonDiscover setBackgroundImage:menuButtonBgPressedPattern forState:UIControlStateHighlighted];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload {
+	[self setMenuButtonDiscover:nil];
+	[super viewDidUnload];
+}
+
+- (IBAction)menuButtonDiscoverTouched:(id)sender
+{
+	[self.sidePanelController showViewController:@"map"];
+}
+@end
