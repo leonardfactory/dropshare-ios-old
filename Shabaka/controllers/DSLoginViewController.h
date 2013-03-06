c933535e65d30f7ffef3e3ba34f5a06a3d3db0a0
Leonardo Ascione <leonardo.ascione@gmail.com>
2013-03-07 00:24:07 +0100

    Added login View

---
+++ Shabaka/controllers/DSLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  DSLoginViewController.h
+//  Shabaka
+//
+//  Created by Leonardo Ascione on 07/03/13.
+//  Copyright (c) 2013 Dropshare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSLoginViewController : UIViewController
+
+@end
---
+++ Shabaka/controllers/DSLoginViewController.m
@@ -0,0 +1,39 @@
+//
+//  DSLoginViewController.m
+//  Shabaka
+//
+//  Created by Leonardo Ascione on 07/03/13.
+//  Copyright (c) 2013 Dropshare. All rights reserved.
+//
+
+#import "DSLoginViewController.h"
+
+@interface DSLoginViewController ()
+
+@end
+
+@implementation DSLoginViewController
+
+- (id) initWithCoder:(NSCoder *)aDecoder
+{
+	self = [super initWithCoder:aDecoder];
+	if(self)
+	{
+		//..
+	}
+	return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
